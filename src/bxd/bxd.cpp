6862822e7177ab76cc60577af02fb7c9b4cee42d
Silvia Amabilino <walfits@gmail.com>
2016-12-19 11:37:14 +0000

    A command BXD is available, but it doesn't do anything

--- src/bxd/bxd.cpp
+++ src/bxd/bxd.cpp
@@ -1,10 +1,12 @@
 #include "/Users/walfits/Repositories/plumed2/src/bias/Bias.h"
 #include "/Users/walfits/Repositories/plumed2/src/bias/ActionRegister.h"
+#include "/Users/walfits/Repositories/plumed2/src/core/ActionAtomistic.h"
 
 #include <string>
 #include <cmath>
 #include <cassert>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -15,26 +17,43 @@ namespace bias{
 class BXD : public Bias
 {
     public:
-        explicit BXD(const ActionOptions&);
-        static void registerKeywords(Keywords& keys);
-        void calculate();
+      explicit BXD(const ActionOptions&);
+      static void registerKeywords(Keywords& keys);
+      void calculate();
+    
+    private:
+      vector<double> box_sep;
+      vector<double> atom_list;
 };
     
+/* This function registers the main keyword for the new BXD class */
+    
 PLUMED_REGISTER_ACTION(BXD,"BXD")
+    
+    
 
+/* This function is used to register any keywords that are needed for BXD to work */
+    
 void BXD::registerKeywords(Keywords& keys)
 {
     Bias::registerKeywords(keys);
 }
 
+    
+/* This reads the values of the keywords that are specified in the input file */
+    
 BXD::BXD(const ActionOptions&ao): PLUMED_BIAS_INIT(ao)
 {
     checkRead();
 }
 
+    
+/* This is the function that does the BXD jobs */
+    
 void BXD::calculate()
 {
-    std::cout << "HELLOOOO!\n";
+    // I want to find a way to obtain all the positions of the atoms
+    
 }
     
     
