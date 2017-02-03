2b484053b160a1d5128916c3c1ef2b1b8d575674
Silvia Amabilino <walfits@gmail.com>
2017-02-03 12:38:04 +0000

    Now the function calculate is called and it prints output to the screen. Inheritance from the action atomistic class has been added.

--- src/bxd/bxd.cpp
+++ src/bxd/bxd.cpp
@@ -1,8 +1,11 @@
-
 #include "/Users/walfits/Repositories/plumed2/src/core/ActionAtomistic.h"
 #include "/Users/walfits/Repositories/plumed2/src/core/ActionWithArguments.h"
 #include "/Users/walfits/Repositories/plumed2/src/core/ActionPilot.h"
+#include "/Users/walfits/Repositories/plumed2/src/core/ActionRegister.h"
+#include "/Users/walfits/Repositories/plumed2/src/core/PlumedMain.h"
+#include "/Users/walfits/Repositories/plumed2/src/core/ActionWithValue.h"
 
+#include "/Users/walfits/Repositories/plumed2/src/bias/Bias.h"
 
 #include "/Users/walfits/Repositories/plumed2/src/tools/AtomNumber.h"
 
@@ -14,31 +17,34 @@
 #include <vector>
 #include <typeinfo>
 #include <algorithm>
-#include <typeinfo>
-
-using namespace std;
-
 
 namespace PLMD{
+namespace bias{
 
 class BXD :
-    public ActionWithArguments,
-    public ActionAtomistic,
-    public ActionPilot
+    public Bias,
+    public ActionAtomistic
 {
     private:
-      double boxSeparation;
-      int numBoxes;
-      vector<double> boxes;
-      bool isFirstStep;
-      void setBoxes(const double);
-      vector<AtomNumber> listAtoms;
+//      double boxSeparation;
+//      int numBoxes;
+//      std::vector<double> boxes;
+//      bool isFirstStep;
+//      void setBoxes(const double);
+    
+      // Resolving the diamond inheritance from ActionAtomistic and ActionWithArguments
+      bool lockRequestArguments;
+      bool lockRequestAtoms;
+      virtual void lockRequests();
+      virtual void unlockRequests();
+      virtual void calculateNumericalDerivatives( ActionWithValue* a=NULL ){};
+    
     
     public:
       explicit BXD(const ActionOptions&);
       static void registerKeywords(Keywords& keys);
-      void calculate();
-      void apply();
+      virtual void calculate();  // pure virutal function that appears in Action.h and has to be defined in the most derived class
+      virtual void apply(){};    // also a pure virtual function
  
 };
     
@@ -46,72 +52,114 @@ class BXD :
     
 PLUMED_REGISTER_ACTION(BXD,"BXD")
     
-    
-
 /* This function is used to register any keywords that are needed for BXD to work */
     
 void BXD::registerKeywords(Keywords& keys)
 {
-    BXD::registerKeywords(keys);
-    keys.use("ARG");                // This enables to use collective variables
-    keys.add("atoms","GROUP","Hacky way to obtain the coordinates of the atoms in the system");
-    keys.add("compulsory","BOXSEP","10.0","Defines the separation between the boxes");
-    keys.add("compulsory","NUMBOXES","10","Defines the number of boxes to use");
+    Bias::registerKeywords(keys);
+    ActionAtomistic::registerKeywords(keys);
+//    keys.use("ARG");                // This enables to use collective variables
+//    keys.add("compulsory","BOXSEP","10.0","Defines the separation between the boxes");
+//    keys.add("compulsory","NUMBOXES","10","Defines the number of boxes to use");
 }
 
     
 /* This reads the values of the keywords that are specified in the input file (it is the constructor) */
     
-BXD::BXD(const ActionOptions&ao):
+BXD::BXD(const ActionOptions& ao):
     Action(ao),
-    ActionAtomistic(ao),
-    ActionPilot(ao),
-    ActionWithArguments(ao),
-    isFirstStep(true)
+    Bias(ao),
+    ActionAtomistic(ao)
+    //isFirstStep(true)
 {
-    parseAtomList("GROUP", listAtoms);
-    parse("BOXSEP", boxSeparation);
-    parse("NUMBOXES", numBoxes);
-    boxes.resize(numBoxes);
+//    parse("BOXSEP", boxSeparation);
+//    parse("NUMBOXES", numBoxes);
+//    boxes.resize(numBoxes);
     checkRead();
+    std::cout << "HELLO from constructor \n";
 }
 
     
-/* This is the function that does the BXD jobs */
+/* This is the function that calculates the values of the variables that will be changed by BXD */
     
 void BXD::calculate()
 {
-    double colVar;
-    colVar = getArgument(0);                    //This puts the value of the first collective variable specified in ARG in colVar
+    std::cout << "HELLO from calculate \n";
+//    double colVar;
+//    colVar = getArgument(0);                    //This puts the value of the first collective variable specified in ARG in colVar
+//    
+//    int natoms;
+//    natoms = plumed.getAtoms().getNatoms();
+//    
+//    std::vector<AtomNumber> index(natoms);
+//    for(int i = 0; i < natoms; i++)
+//    {
+//        index[i].setIndex(i);
+//    }
+//    requestAtoms(index);
+//    
+//    Vector aPosition;
+//    aPosition = getPosition(6);
+//
+//    std::cout << aPosition[0] << std::endl;
+//
+//    void * posPtr;
+//    posPtr = &aPosition[0];
+//    
+//    plumed.getAtoms().setPositions(posPtr,1);
     
-    setBoxes(colVar);
     
-    vector<double> positions;
-    positions.resize(3);
-    positions = getPositions()
     
-    cout << typeid(listAtoms[21]).name() << endl;
-    
-
-
+//    if(isFirstStep)
+//    {
+//        setBoxes(colVar);
+//    }
     
 }
+    
+/* This is the function that applies the changes calculated in the function 'calculate'. Both 'apply' and 'calculate' are virtual functions that are first introduced in Action.h and then have to be defined in the derived classes. */
+    
+//void apply()
+//{
+//    std::cout << "HELLO";
+//}
     
     
 /* This function uses the first value of the collective variable to set an array of barriers that define the boxes. */
     
-void BXD::setBoxes(const double firstColVar)
+//void BXD::setBoxes(const double firstColVar)
+//{
+//    boxes[0] = firstColVar - 0.5 * boxSeparation;
+//    
+//    for(int i = 1; i < numBoxes; i++)
+//    {
+//        boxes[i] = i*boxSeparation;
+//    }
+//}
+    
+    
+/* Resolving the diamond inheritance problem. The virtual functions 'lockRequest' and 'unlockRequest' appear in the base class Action and in the two child classes ActionWithArguments and ActionAtomistic. BXD is the only class so far in PLUMED that inherits from both ActionWithArguments and ActionAtomistic, so a final overrider has to be present. For now, I just joined the two functions that were in ActionWithArguments and ActionAtomistic into one. */
+    
+void BXD::lockRequests()
 {
-    boxes[0] = firstColVar - 0.5 * boxSeparation;
-    
-    for(int i = 1; i < numBoxes; i++)
-    {
-        boxes[i] = i*boxSeparation;
-    }
-    
+    lockRequestArguments=true;
+    lockRequestAtoms=true;
 }
-   
+
+void BXD::unlockRequests()
+{
+    lockRequestArguments=false;
+    lockRequestAtoms=false;
+}
     
+/* This is again a problem of diamond inheritance.  */
+    
+//void BXD::calculateNumericalDerivatives( ActionWithValue* a )
+//{
+//    
+//}
+//    
     
 // Brackets from the namespaces!
+}
 }
